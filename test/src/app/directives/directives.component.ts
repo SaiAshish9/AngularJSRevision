47c82fabf6aaa717f129051fc9adc40b1d0ca6cb
SaiAshish9 <saiashish3760@gmail.com>
2020-08-27 12:07:13 +0530

    ngFor exported values and change detection updated

--- test/src/app/directives/directives.component.ts
+++ test/src/app/directives/directives.component.ts
@@ -8,4 +8,23 @@ import { Component, OnInit } from '@angular/core';
 export class DirectivesComponent {
   courses = [1, 2];
   viewMode = 'map';
+  onAdd() {
+    this.loopData.push({
+      id: 2,
+      name: 'course3',
+    });
+  }
+  onChange(course) {
+    course.name = 'UPDATED';
+    // let index=this.loopData.indexOf(course);
+    // this.loopData.splice(index,1);
+  }
+  onRemove(course) {
+    let index=this.loopData.indexOf(course);
+    this.loopData.splice(index,1);
+    }
+  loopData = [
+    { id: 0, name: 'list option 1' },
+    { id: 1, name: 'list option 2' },
+  ];
 }
