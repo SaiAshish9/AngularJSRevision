4a505b5941806b655c7d3152d2ab024633805c3b
SaiAshish9 <saiashish3760@gmail.com>
2020-08-28 18:13:21 +0530

    formGroupName implemented

--- test/src/app/reactive-form/reactive-form.component.ts
+++ test/src/app/reactive-form/reactive-form.component.ts
@@ -10,28 +10,30 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class ReactiveFormComponent {
   form = new FormGroup({
-    username: new FormControl(
-      ' ',
-      [
-        Validators.required,
-        Validators.minLength(3),
-        UsernameValidators.cannotContainSpace,
-      ],
-      UsernameValidators.shouldBeUnique
-    ),
-    password: new FormControl('', Validators.required),
+    account: new FormGroup({
+      username: new FormControl(
+        ' ',
+        [
+          Validators.required,
+          Validators.minLength(3),
+          UsernameValidators.cannotContainSpace,
+        ],
+        UsernameValidators.shouldBeUnique
+      ),
+      password: new FormControl('', Validators.required),
+    }),
   });
 
   login() {
     // let isValid = authService.login(this.form.value);
     // if(!isValid) {
-      this.form.setErrors({
-        inValigLogin: true,
-      })
+    this.form.setErrors({
+      inValigLogin: true,
+    });
     // }
   }
 
   get username() {
-    return this.form.get('username');
+    return this.form.get('account.username');
   }
 }
