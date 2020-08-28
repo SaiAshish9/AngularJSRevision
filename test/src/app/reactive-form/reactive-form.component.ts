ae941c8121adb7ad866397df1de5bd34a64fe2cc
SaiAshish9 <saiashish3760@gmail.com>
2020-08-28 17:09:41 +0530

    updated asynchronous validators

---
+++ test/src/app/reactive-form/reactive-form.component.ts
@@ -0,0 +1,27 @@
+import { UsernameValidators } from './username.validators';
+import { Component, OnInit } from '@angular/core';
+
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-reactive-form',
+  templateUrl: './reactive-form.component.html',
+  styleUrls: ['./reactive-form.component.css'],
+})
+export class ReactiveFormComponent {
+  form = new FormGroup({
+    username: new FormControl(
+      ' ',
+      [
+        Validators.required,
+        Validators.minLength(3),
+        UsernameValidators.cannotContainSpace,
+      ],
+      UsernameValidators.shouldBeUnique
+    ),
+    password: new FormControl('', Validators.required),
+  });
+  get username() {
+    return this.form.get('username');
+  }
+}
